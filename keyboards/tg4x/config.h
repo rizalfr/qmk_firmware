0c8d941eb6b37f518053f2f270d8b1cd5b19a39b
rizalfr <zacksupreme@gmail.com>
2021-01-26 13:29:27 +0700

    tg4x change led numbers

--- keyboards/tg4x/config.h
+++ keyboards/tg4x/config.h
@@ -57,15 +57,19 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define BACKLIGHT_PIN B7
 // #define BACKLIGHT_BREATHING
 // #define BACKLIGHT_LEVELS 3
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+#define RGBLIGHT_LAYERS_OVERRIDE_RGB_OFF
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_MAX_LAYERS 8
 #define RGB_DI_PIN D2
 #ifdef RGB_DI_PIN
-  #define RGBLED_NUM 6
+  #define RGBLED_NUM 7
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
   #define RGBLIGHT_LIMIT_VAL 255 /* The maximum brightness level */
   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+
 /*== all animations enable ==*/
   #define RGBLIGHT_ANIMATIONS
 /*== or choose animations ==*/
