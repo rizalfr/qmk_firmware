768b130cdceff61cb9a54e71bc447f6f4ff9ac08
Rizal Fauzi Rahman <zacksupreme@gmail.com>
2021-12-27 09:04:14 +0700

    add diwhy

---
+++ keyboards/diwhy/keymaps/default/keymap.c
@@ -0,0 +1,19 @@
+#include "kb.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[0] = LAYOUT_all(
+		KC_NLCK, KC_SLSH, KC_ASTR, KC_MINS,
+		KC_7, KC_8, KC_9,
+		KC_4, KC_5, KC_6, KC_PLUS,
+		KC_1, KC_2, KC_3,
+		KC_0, KC_DOT, KC_ENT),
+
+	[1] = LAYOUT_all(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS),
+};
+
