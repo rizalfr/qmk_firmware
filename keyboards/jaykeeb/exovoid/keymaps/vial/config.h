6ca196d46dc1f7a4d0cf280f05f87022f0a6d5ee
zacksupreme <zacksupreme@gmail.com>
2025-09-11 00:12:35 +0700

    masukin kibot armas

---
+++ keyboards/jaykeeb/exovoid/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF7, 0x3B, 0xBF, 0x52, 0xB2, 0xBB, 0xD0, 0x33}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
---
+++ keyboards/jaykeeb/void65_hotswap/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x7F, 0x55, 0x19, 0x37, 0xDC, 0x95, 0x16, 0xCA}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
